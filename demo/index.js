92d7eb69351feb8d8ec212f50009df51d70099c1
Finger Lickin Jesus <59297610+shawtylikeamelody@users.noreply.github.com>
2021-07-26 17:14:27 -0400

    S

--- demo/index.js
+++ demo/index.js
@@ -1,19 +1,17 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 const ssl = {
-    key: fs.readFileSync('./ssl.key'),
-    cert: fs.readFileSync('./ssl.cert'),
+    key: fs.readFileSync(path.join(__dirname, '/ssl.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/ssl.cert')),
 };
 const server = https.createServer(ssl);
 const Corrosion = require('../');
 const proxy = new Corrosion({
-    codec: 'base64',
+    codec: 'xor',
 });
-
-// Builds client injection script.
-proxy.bundleScripts();
 
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', proxy.upgrade).listen(443); 
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(443);
